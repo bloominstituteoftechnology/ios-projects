1213a4e5c669a8f516f88b967337c70d4e04d6b6
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-28 18:29:24 -0400

    Add touch handlers

--- Sprint 6/Module 2-Custom Controls/Ratings/Ratings/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/Ratings/Ratings/CustomControl.swift
@@ -42,6 +42,45 @@ import UIKit
         return CGSize(width: width, height: componentDimension)
     }
     
+    // MARK: - Touch Handling
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    func updateValue(at touch: UITouch) {
+        
+        
+        
+    }
+    
     // MARK: - Properties
     
     var value: Int = 1
