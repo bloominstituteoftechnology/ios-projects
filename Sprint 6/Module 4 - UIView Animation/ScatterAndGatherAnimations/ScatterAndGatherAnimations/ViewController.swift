8e37544ea6633b875f430d8a8ecf565ed5ca115b
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-29 19:30:23 -0400

    Hook up scatter and gather

--- Sprint 6/Module 4 - UIView Animation/ScatterAndGatherAnimations/ScatterAndGatherAnimations/ViewController.swift
+++ Sprint 6/Module 4 - UIView Animation/ScatterAndGatherAnimations/ScatterAndGatherAnimations/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        labels = [lambdaL, lambdaAOne, lambdaM, lambdaB, lambdaD, lambdaATwo]
+        
         view.addSubview(lambdaL)
         view.addSubview(lambdaAOne)
         view.addSubview(lambdaM)
@@ -25,19 +27,40 @@ class ViewController: UIViewController {
     @IBAction func toggle(_ sender: Any) {
         
         if shouldScramble == false {
+            
+            UIView.animate(withDuration: 3.0) {
+                self.lambdaLogo.alpha = 0.0
+                
+                for label in self.labels {
+                    let index = Int(arc4random_uniform(UInt32(self.colorArray.count)))
+                    label.textColor = self.colorArray[index]
+                    let backgroundIndex = Int(arc4random_uniform(UInt32(self.colorArray.count)))
+                    label.layer.backgroundColor = self.colorArray[backgroundIndex].cgColor
+                    label.flare()
+                }
+            }
+            
             shouldScramble = true
         } else {
+            
+            UIView.animate(withDuration: 3.0) {
+                self.lambdaLogo.alpha = 1.0
+                
+                for label in self.labels {
+                    label.textColor = .black
+                    label.layer.backgroundColor = UIColor.clear.cgColor
+                    label.unflare()
+                }
+            }
+            
             shouldScramble = false
-        }
-        
-        if shouldScramble == true {
-            
-        } else {
-            
         }
     }
     
     var shouldScramble: Bool = false
+    let colorArray = [UIColor.red, UIColor.orange, UIColor.blue, UIColor.cyan, UIColor.purple, UIColor.green, UIColor.magenta, UIColor.yellow]
+    
+    private var labels: [UILabel] = []
 
     @IBOutlet weak var lambdaL: UILabel!
     @IBOutlet weak var lambdaAOne: UILabel!
@@ -46,6 +69,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var lambdaD: UILabel!
     @IBOutlet weak var lambdaATwo: UILabel!
     @IBOutlet weak var lambdaLogo: UIImageView!
+    
+}
+
+extension UIView {
+    
+    func flare() {
+        let translate = CGAffineTransform(translationX: CGFloat(arc4random_uniform(UInt32(superview?.bounds.width ?? 300))) - frame.minX, y: CGFloat(arc4random_uniform(UInt32(superview?.bounds.height ?? 300))) - frame.minY)
+        transform = translate.rotated(by: CGFloat(arc4random_uniform(361)) / CGFloat.pi*2).scaledBy(x: 1.6, y: 1.6)
+//
+//        let rotate = CGAffineTransform(rotationAngle: CGFloat(arc4random_uniform(361)) / CGFloat.pi*2)
+//        transform = rotate.translatedBy(x: CGFloat(arc4random_uniform(UInt32(superview?.bounds.width ?? 300))), y: CGFloat(arc4random_uniform(UInt32(superview?.bounds.height ?? 300))))
+    }
+    
+    func unflare() {
+        transform = .identity
+    }
     
 }
 
