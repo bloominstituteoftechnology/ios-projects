8e8ffb12dc1aaa4e28ca165881ba7ad179fbd79d
stdemicheli <stdemicheli@gmail.com>
2018-09-06 01:46:38 +0200

    Implemented Waiter solution.

--- Sprint 7/hungry-developers/hungry-developers/HungryDevs_Arbitrator.swift
+++ Sprint 7/hungry-developers/hungry-developers/HungryDevs_Arbitrator.swift
@@ -5,9 +5,10 @@
 //  Created by De MicheliStefano on 05.09.18.
 //  Copyright © 2018 De MicheliStefano. All rights reserved.
 //
+
 import Foundation
 
-func initHungryDevelopers() {
+func initHungryDevelopers1() {
     var developers: [Developer] = stride(from: 0, to: 5, by: 1).map { i in Developer(with: i) }
     var spoons: [Spoon] = stride(from: 0, to: 5, by: 1).map { i in Spoon(with: i) }
     
@@ -17,13 +18,58 @@ func initHungryDevelopers() {
     developers[2].rightSpoon = spoons[3]; developers[3].leftSpoon = spoons[3]
     developers[3].rightSpoon = spoons[4]; developers[4].leftSpoon = spoons[4]
     
+    let waiter = Waiter()
+    waiter.spoons = spoons
+    
     DispatchQueue.concurrentPerform(iterations: 5) {
         developers[$0].run()
     }
 }
 
+class Waiter {
 
-class Spoon {
+    static let shared = Waiter()
+    var spoons: [Spoon]!
+
+    func askPermission(toUse spoon1: Spoon, and spoon2: Spoon) -> Bool {
+        if checkAvailability(for: spoon1) && checkAvailability(for: spoon2) {
+            DispatchQueue.main.sync {
+                spoon1.isAvailable = false
+                spoon2.isAvailable = false
+            }
+            
+            return true
+        }
+        return false
+    }
+    
+    func returnSpoons(_ spoon1: Spoon, _ spoon2: Spoon) {
+        DispatchQueue.main.sync {
+            spoon1.isAvailable = true
+            spoon2.isAvailable = true
+        }
+    }
+    
+    private func checkAvailability(for spoonLookup: Spoon) -> Bool {
+        for spoon in spoons {
+            if spoon.index == spoonLookup.index && spoon.isAvailable {
+                return true
+            }
+        }
+        return false
+    }
+    
+//    private func setAsAvailable(for spoonLookup: Spoon) {
+//        for spoon in spoons {
+//            if spoon.index == spoonLookup.index {
+//                // do mutext: set to available
+//            }
+//        }
+//    }
+
+}
+
+class Spoon1 {
     
     var index: Int
     var isAvailable: Bool = true
@@ -47,7 +93,7 @@ class Spoon {
     
 }
 
-class Developer {
+class Developer1 {
     
     var number: Int
     var usingLeftSpoon: Bool = false
@@ -62,21 +108,11 @@ class Developer {
     func think() {
         // continue thinking while either leftspoon or rightspoon are not yet available
         repeat {
-            if leftSpoon.isAvailable && rightSpoon.isAvailable {
-                if leftSpoon.index < rightSpoon.index {
-                    leftSpoon.pickUp(); usingLeftSpoon = true
-                } else {
-                    rightSpoon.pickUp(); usingRightSpoon = true
-                }
-            }
+            let permissionGranted = Waiter.shared.askPermission(toUse: leftSpoon, and: rightSpoon)
             
-            if leftSpoon.isAvailable { leftSpoon.pickUp(); usingLeftSpoon = true }
-            if rightSpoon.isAvailable { rightSpoon.pickUp(); usingRightSpoon = true }
-            
-        } while !usingLeftSpoon && !usingRightSpoon
+            if permissionGranted { return }
+        } while true
         
-        // when both spoons have been picked up then return to eating
-        return
     }
     
     func eat() {
@@ -86,8 +122,7 @@ class Developer {
         usleep(5)
         
         // after having eaten a hearty meal, put down them spoons and return to thinking
-        usingLeftSpoon = false; usingRightSpoon = false
-        leftSpoon.putDown(); rightSpoon.putDown()
+        Waiter.shared.returnSpoons(leftSpoon, rightSpoon)
         
         print("Developer \(number) finished eating")
         return
