5dc1e4ca10239f90416f1ecb9a5102ad59446bfa
stdemicheli <stdemicheli@gmail.com>
2018-09-06 14:43:39 +0200

    Refactored waiter/arbitrator solution.

--- Sprint 7/hungry-developers/hungry-developers/HungryDevs_Arbitrator.swift
+++ Sprint 7/hungry-developers/hungry-developers/HungryDevs_Arbitrator.swift
@@ -9,70 +9,49 @@
 import Foundation
 
 func initHungryDevelopers1() {
-    var developers: [Developer] = stride(from: 0, to: 5, by: 1).map { i in Developer(with: i) }
-    var spoons: [Spoon] = stride(from: 0, to: 5, by: 1).map { i in Spoon(with: i) }
-    
+    var developers: [Developer1] = stride(from: 0, to: 5, by: 1).map { i in Developer1(with: i) }
+    var spoons: [Spoon1] = stride(from: 0, to: 5, by: 1).map { i in Spoon1(with: i) }
+
     developers[4].rightSpoon = spoons[0]; developers[0].leftSpoon = spoons[0]
     developers[0].rightSpoon = spoons[1]; developers[1].leftSpoon = spoons[1]
     developers[1].rightSpoon = spoons[2]; developers[2].leftSpoon = spoons[2]
     developers[2].rightSpoon = spoons[3]; developers[3].leftSpoon = spoons[3]
     developers[3].rightSpoon = spoons[4]; developers[4].leftSpoon = spoons[4]
-    
-    let waiter = Waiter()
-    waiter.spoons = spoons
-    
-    DispatchQueue.concurrentPerform(iterations: 5) {
-        developers[$0].run()
+
+    //let waiter = Waiter()
+
+    for dev in developers {
+        DispatchQueue.global().async {
+            dev.run()
+        }
     }
+    
+    dispatchMain()
 }
 
 class Waiter {
 
     static let shared = Waiter()
-    var spoons: [Spoon]!
+    private let lock = NSLock()
 
-    func askPermission(toUse spoon1: Spoon, and spoon2: Spoon) -> Bool {
-        if checkAvailability(for: spoon1) && checkAvailability(for: spoon2) {
-            DispatchQueue.main.sync {
-                spoon1.isAvailable = false
-                spoon2.isAvailable = false
-            }
-            
-            return true
-        }
-        return false
-    }
-    
-    func returnSpoons(_ spoon1: Spoon, _ spoon2: Spoon) {
-        DispatchQueue.main.sync {
-            spoon1.isAvailable = true
-            spoon2.isAvailable = true
-        }
-    }
-    
-    private func checkAvailability(for spoonLookup: Spoon) -> Bool {
-        for spoon in spoons {
-            if spoon.index == spoonLookup.index && spoon.isAvailable {
-                return true
-            }
-        }
-        return false
-    }
-    
-//    private func setAsAvailable(for spoonLookup: Spoon) {
-//        for spoon in spoons {
-//            if spoon.index == spoonLookup.index {
-//                // do mutext: set to available
-//            }
+    func askPermission(toUse spoon1: Spoon1, and spoon2: Spoon1){
+        lock.lock()
+        spoon1.pickUp()
+        spoon2.pickUp()
+//        if spoon1.index < spoon2.index {
+//            spoon1.pickUp(); spoon2.pickUp()
+//        } else {
+//            spoon2.pickUp(); spoon1.pickUp()
 //        }
-//    }
+        lock.unlock()
+    }
 
 }
+
 
 class Spoon1 {
     
     var index: Int
-    var isAvailable: Bool = true
     private let lock = NSLock()
     
     init(with index: Int) {
@@ -80,15 +59,15 @@ class Spoon1 {
     }
     
     func pickUp() {
+        print("Spoon \(index) is being picked up")
         lock.lock()
-        isAvailable = false
-        lock.unlock()
+        print("Spoon \(index) was picked up")
     }
     
     func putDown() {
-        lock.lock()
-        isAvailable = true
+        print("Spoon \(index) is being put down")
         lock.unlock()
+        print("Spoon \(index) was put down")
     }
     
 }
@@ -96,35 +75,29 @@ class Spoon1 {
 class Developer1 {
     
     var number: Int
-    var usingLeftSpoon: Bool = false
-    var usingRightSpoon: Bool = false
-    var leftSpoon: Spoon!
-    var rightSpoon: Spoon!
+    var leftSpoon: Spoon1!
+    var rightSpoon: Spoon1!
     
     init(with index: Int) {
         self.number = index
     }
     
     func think() {
-        // continue thinking while either leftspoon or rightspoon are not yet available
-        repeat {
-            let permissionGranted = Waiter.shared.askPermission(toUse: leftSpoon, and: rightSpoon)
-            
-            if permissionGranted { return }
-        } while true
-        
+        print("Developer \(number) asking for Permission")
+        Waiter.shared.askPermission(toUse: leftSpoon, and: rightSpoon)
+        print("Developer \(number) picked up spoons \(leftSpoon.index) and \(rightSpoon.index)")
+        return
     }
     
     func eat() {
-        print("Developer \(number) started eating")
         
-        // enjoy a quick bite for 5 microseconds
+        print("Developer \(number) started eating with spoons \(leftSpoon.index) and \(rightSpoon.index)")
         usleep(5)
         
-        // after having eaten a hearty meal, put down them spoons and return to thinking
-        Waiter.shared.returnSpoons(leftSpoon, rightSpoon)
+        leftSpoon.putDown()
+        rightSpoon.putDown()
+        print("Developer \(number) finished eating with spoons \(leftSpoon.index) and \(rightSpoon.index)")
         
-        print("Developer \(number) finished eating")
         return
     }
     
