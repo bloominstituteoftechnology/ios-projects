e41c8bc8449a0660941e6d5f6593f4db9c4adc98
Moin Uddin <U_Moin75@yahoo.com>
2018-10-02 17:34:11 -0400

    Create CustomControl class

---
+++ Sprint 6/Module 2-Custom Controls/Stars Control/Stars Control/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  Stars Control
+//
+//  Created by Moin Uddin on 10/2/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value: Int = 1
+}
