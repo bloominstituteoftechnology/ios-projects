8d447ae46bcc860094f17524ca599a25f8882693
Daniela Parra <dyparra47@gmail.com>
2018-10-02 16:14:20 -0400

    Set up storyboard with custom control class.

---
+++ Sprint 6/Module 2-Custom Controls/CustomUIControls/CustomUIControls/CustomControl.swift
@@ -0,0 +1,20 @@
+//
+//  CustomControl.swift
+//  CustomUIControls
+//
+//  Created by Daniela Parra on 10/2/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    
+    
+    
+    
+    var value: Int = 1
+    
+    
+}
