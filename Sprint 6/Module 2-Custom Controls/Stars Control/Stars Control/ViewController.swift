dfdfadb224849e29aeac7f3e1748a7597e90e893
Moin Uddin <U_Moin75@yahoo.com>
2018-10-02 17:46:59 -0400

    Setup Storyboard, UIView, and CustomControl IBaction

--- Sprint 6/Module 2-Custom Controls/Stars Control/Stars Control/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/Stars Control/Stars Control/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        title = "User Rating: N stars"
+    }
+    
 
 }
 
