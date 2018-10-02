21b93c2e4815a9e287501e600e42118267490465
Daniela Parra <dyparra47@gmail.com>
2018-10-02 17:19:50 -0400

    Add touch handlers.

--- Sprint 6/Module 2-Custom Controls/CustomUIControls/CustomUIControls/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/CustomUIControls/CustomUIControls/CustomControl.swift
@@ -49,6 +49,48 @@ class CustomControl: UIControl {
         return CGSize(width: width, height: componentDimension)
     }
     
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        
+        sendActions(for: [.touchDown, .valueChanged])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            
+            sendActions(for: [.touchUpInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    func updateValue(at touch: UITouch) {
+        
+    }
+    
     let componentDimension: CGFloat = 40.0
     let componentCount = 5
     
