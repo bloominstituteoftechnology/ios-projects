7c812c2a55af4ffb2e0de96f00b5d93c74a6efd8
Dillon McElhinney <dillon.mce@gmail.com>
2018-10-02 06:37:23 -0600

    Build custom control view

--- Sprint 6/Module 2-Custom Controls/Custom Star Rating Control/Custom Star Rating Control/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Star Rating Control/Custom Star Rating Control/CustomControl.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class CustomControl: UIControl {
     var value: Int = 1
+    
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    
+    required init? (coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        
+        setup()
+    }
+    
+    private func setup() {
+        var labels: [UILabel] = []
+        for i in 1...componentCount {
+            let label = UILabel()
+            self.addSubview(label)
+            labels.append(label)
+            label.tag = i
+            let x = componentDimension * CGFloat(i-1) + CGFloat(8 * i)
+            label.frame = CGRect(x: x, y: 0, width: componentDimension, height: componentDimension)
+            label.font = UIFont.boldSystemFont(ofSize: 32)
+            label.text = "☆"
+            label.textAlignment = .center
+            label.textColor = i == 1 ? componentActiveColor : componentInactiveColor
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
 }
