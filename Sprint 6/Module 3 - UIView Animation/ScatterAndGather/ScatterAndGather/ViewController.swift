4b903ef7dd84934c255dee7566ecd611d826ed38
Daniela Parra <dyparra47@gmail.com>
2018-10-03 19:36:45 -0400

    Add shadow to animation.

--- Sprint 6/Module 3 - UIView Animation/ScatterAndGather/ScatterAndGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -9,21 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //setUp()
-    }
-    
-    private func setUp() {
-        
-        let logoCenterXConstraint = NSLayoutConstraint(item: lambdaImageView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
-        
-        let logoCenterYConstraint = NSLayoutConstraint(item: lambdaImageView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: -20)
-        
-        NSLayoutConstraint.activate([logoCenterXConstraint, logoCenterYConstraint])
-    }
     
     @IBAction func toggle(_ sender: UIBarButtonItem) {
         let labels: [UILabel] = [self.lLabel, self.a1Label, self.mLabel, self.bLabel, self.dLabel, self.a2Label]
@@ -32,10 +17,14 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 2, delay: 0, options: .curveEaseOut, animations: {
                 
                 for label in labels {
+                    
+                    //Translation x and y
                     let superViewWidth = self.view.frame.size.width
-                    let randomX = CGFloat.random(in: 0..<superViewWidth)
+                    let randomX = CGFloat.random(in: 0..<(superViewWidth - label.frame.size.width - label.frame.origin.x))
                     let superViewHeight = self.view.frame.size.height
-                    let randomY = CGFloat.random(in: 0..<superViewHeight)
+                    let randomY = CGFloat.random(in: 0..<(superViewHeight - label.frame.size.height - label.frame.origin.y))
+                    
+                    //Colors and Angles
                     let randomRadian = CGFloat.random(in: 0...CGFloat.pi)
                     let randomTextColor = UIColor.random
                     let randomColor = UIColor.random
@@ -43,13 +32,10 @@ class ViewController: UIViewController {
                     label.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
                     label.textColor = randomTextColor
                     label.backgroundColor = randomColor.withAlphaComponent(0.5)
+                    label.layer.shadowColor = UIColor.black.cgColor
+                    label.layer.shadowOpacity = 0.5
+                    label.layer.shadowOffset = CGSize(width: 4, height: 4)
                 }
-//                self.lLabel.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
-//                self.a1Label.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
-//                self.mLabel.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
-//                self.bLabel.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
-//                self.dLabel.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
-//                self.a2Label.transform = CGAffineTransform.init(translationX: randomX, y: randomY).rotated(by: randomRadian)
                 
                 //Makes logo transparent.
                 self.lambdaImageView.alpha = 0
@@ -63,6 +49,7 @@ class ViewController: UIViewController {
                     label.transform = .identity
                     label.textColor = UIColor.black
                     label.backgroundColor = UIColor.clear
+                    label.layer.shadowOpacity = 0
                 }
                 
                 self.lambdaImageView.alpha = 1
@@ -75,7 +62,6 @@ class ViewController: UIViewController {
     // MARK: - Properties
     
     var shouldScramble = true
-    let colors: [UIColor] = [.black, .brown, .blue, .cyan, .darkGray, .gray, .green, .lightGray, .magenta, .orange, .purple, .red, .yellow]
     
     @IBOutlet weak var lLabel: UILabel!
     @IBOutlet weak var a1Label: UILabel!
