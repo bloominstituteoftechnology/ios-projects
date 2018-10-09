cd3319dde794b704dccee0f076a9a1dc33d0e26f
iyinraphael <iyinraphael@gmail.com>
2018-10-09 14:45:34 -0700

    counted struct created

---
+++ Sprint 6/Module 4 - View Controller Transitions/Friends Animation/Friends Animation/Model Controller /NavigationControllerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationControllerDelegate.swift
+//  Friends Animation
+//
+//  Created by Iyin Raphael on 10/4/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
