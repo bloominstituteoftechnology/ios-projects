f7625eeaca38477d627f56aff7f35a2c842ac9fc
Moin Uddin <U_Moin75@yahoo.com>
2018-10-24 19:33:01 -0400

    Implement LoadingViewController and use the framework in ViewController

--- Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
+++ Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 public class LoadingViewController: UIViewController {
-    let i = IndeterminateLoadingView()
+    
+    
+    public override func viewDidLoad() {
+        view.backgroundColor = .darkGray
+        view.addSubview(loadingView)
+        centerLoadingView()
+    }
+    
+    private func centerLoadingView() {
+        loadingView.center = view.convert(view.center, from: view.superview)
+    }
+    
+    public func startAnimation() {
+        loadingView.startAnimating()
+    }
+    
+    private(set) var loadingView = IndeterminateLoadingView(frame: CGRect(x:0, y: 0, width: 150, height: 150))
 }
--- Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
+++ Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateButtonLabel.center = view.convert(view.center, from: view.superview)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Loading" {
+            guard let loadingView = segue.destination as? LoadingViewController else { return }
+            loadingView.startAnimation()
+        }
+    }
 
-
+    @IBOutlet weak var animateButtonLabel: UIButton!
+    
 }
 
