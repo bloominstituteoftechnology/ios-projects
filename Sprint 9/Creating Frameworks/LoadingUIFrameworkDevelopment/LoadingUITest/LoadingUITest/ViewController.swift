9c599541a32d329f899873b54c12ea4b2c2b3d3c
Dillon McElhinney <dillon.mce@gmail.com>
2018-10-24 13:05:54 -0600

    Make initial Xcode projects for LoadingUI framework.

---
+++ Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Dillon McElhinney on 10/24/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
