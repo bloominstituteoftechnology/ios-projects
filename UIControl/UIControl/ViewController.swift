4cb7c743c8a36d82eabd5775322a573e0178c535
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-13 10:41:43 -0800

    initial commit

---
+++ UIControl/UIControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIControl
+//
+//  Created by Jerrick Warren on 11/13/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
