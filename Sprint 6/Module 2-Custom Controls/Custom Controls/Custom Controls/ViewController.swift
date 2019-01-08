24636b37ba9a24a4827a0c5cfb25011506a74aba
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-08 14:01:01 -0800

    update the navigation title based on the amount of stars selected
    This currently not functional.

--- Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
@@ -3,7 +3,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func updateRating(_ sender: CustomControl) {
-        self.navigationItem.title = "User Rating: N Stars"
+        var starCount = 0
+        if startCount == 1 {
+        self.navigationItem.title = "User Rating: 1 Star"
+        } else {
+            self.navigationItem.title = "User Rating: \(starCount) Stars"
+        }
     }
 }
 
