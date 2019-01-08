a5858007dde73bf2b38b1caf445b4c0f475bf703
Frulwinn <frulwinn@gmail.com>
2019-01-08 13:41:55 -0800

    added private constants to customcontrol

--- Sprint 6/Module 2-Custom Controls/UIControl/UIControl/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/UIControl/UIControl/CustomControl.swift
@@ -2,4 +2,9 @@ import UIKit
 
 class CustomControl: UIControl {
     var value: Int = 1
+    
+    private static let componentDimension: CGFloat = 40.0
+    private static let componentCount = 5
+    private static let componentActiveColor = UIColor.black
+    private static let componentInactiveColor = UIColor.gray
 }
--- Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
@@ -2,12 +2,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func updateRating(_ sender: Any) {
+    @IBAction func updateRating(ratingControl: CustomControl) {
+//  Implement `updateRating`. Set the view controller's title to the string
+//  `"User Rating: N stars"` where N is the number of stars. This number is the   control's visible `value` property.
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
+        
     }
 }
 
