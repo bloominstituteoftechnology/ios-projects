0c5a1adc9e59329d6df8ef3ad8d988591a38c743
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-08 14:43:40 -0800

    build the control view
    - minor typo fixed on the VC
    - added pseudocode to the Custom Control

--- Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/CustomControl.swift
@@ -2,4 +2,47 @@ import UIKit
 
 class CustomControl: UIControl {
     var value: Int = 1
+    
+    // private constants
+    
+    private let componentDimension: CGFloat = 40
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init().CustomControl.setup()
+    }
+    
+    var labelArray: [UILabel] = []
+    
+    func setup() {
+        for index in 0..<ViewController.starCount {
+            print("shut up, xcode")
+            var newLabel = create.UILabel
+            labelArray.append(newLabel)
+            let tag = labelArray[indexPath.row]
+            label.frameSize = [componentDimension, componentDimension]
+            func labelTray {
+                UIStackView.labelArray.spacing(8)
+                UIStackView.labelArray.initialSpacing(8)
+            }
+            label.font = .systemBold
+            label.fontSize = 32
+            label.text = *
+            label.alignment = center
+            if label == active {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
 }
--- Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
@@ -3,8 +3,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func updateRating(_ sender: CustomControl) {
-        var starCount = 0
-        if startCount == 1 {
+        static var starCount = 0
+        if starCount == 1 {
         self.navigationItem.title = "User Rating: 1 Star"
         } else {
             self.navigationItem.title = "User Rating: \(starCount) Stars"
