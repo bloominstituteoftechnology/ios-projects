758ba79d11ed0770108e8ac982bdab667b58984f
Frulwinn <frulwinn@gmail.com>
2019-01-08 12:55:33 -0800

    added connected ibaction from view to viewcontroller

--- Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func updateRating(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
