81bf5ca6bab4af76789b531a6490566439ac7438
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-01-09 10:27:04 -0800

    Scatter & Gather: Setup

---
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather Animations
+//
+//  Created by Ivan Caldwell on 1/9/19.
+//  Copyright © 2019 Ivan Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
