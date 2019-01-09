843aefcd090e2b007101038e8ccf0f813a8740de
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-09 09:42:58 -0800

    create new project and add new branch

---
+++ Sprint 6/Module 3 - UIView Animation/UIAnimation/UIAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIAnimation
+//
+//  Created by Sergey Osipyan on 1/9/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
