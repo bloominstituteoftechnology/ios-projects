cefc19b5d91e1bf5525ac649243510e083bf005c
Frulwinn <frulwinn@gmail.com>
2019-01-09 15:42:29 -0800

    animated label d

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -47,30 +47,49 @@ class ViewController: UIViewController {
             //animating label m
             UIView.animate(withDuration: 1.0) {
                 view3?.transform = CGAffineTransform(scaleX: 20.0, y: 20.0)
-                
                 view3?.frame.origin.x += 100
                 view3?.frame.origin.y += 300
+                
                 view3?.backgroundColor = self.getRandomColor()
                 view3?.textColor = self.getRandomColor()
             }
             //returning label m
             UIView.animate(withDuration: 1.0) {
-                
+
                 view3?.transform = .identity
             }
-            //animating label d
+            //animating label b
             UIView.animate(withDuration: 1.0) {
-                view3?.transform = CGAffineTransform(scaleX: 20.0, y: 20.0)
+                view4?.transform = CGAffineTransform(scaleX: 20.0, y: 20.0)
                 
-                view3?.frame.origin.x += 300
-                view3?.frame.origin.y += 100
-                view3?.backgroundColor = self.getRandomColor()
-                view3?.textColor = self.getRandomColor()
+                view4?.frame.origin.x -= 100
+                view4?.frame.origin.y -= 300
+                view4?.backgroundColor = self.getRandomColor()
+                view4?.textColor = self.getRandomColor()
             }
-            //returning label d
+            //returning label b
             UIView.animate(withDuration: 1.0) {
+
+                view4?.transform = .identity
+            }
+            //animating d
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.4) {
+                view5?.center = self.view.center
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.2) {
+                view5?.transform = CGAffineTransform(scaleX: 6.7, y: 5.6)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.2) {
+                view5?.transform = CGAffineTransform(scaleX: 5.6, y: 6.7)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.7, relativeDuration: 0.15) {
+                view5?.transform = CGAffineTransform(scaleX: 6.11, y: 5.9)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.85, relativeDuration: 0.15) {
+                view5?.transform = .identity
                 
-                view3?.transform = .identity
+                view5?.backgroundColor = self.getRandomColor()
+                view5?.textColor = self.getRandomColor()
             }
         }
         
@@ -82,15 +101,11 @@ class ViewController: UIViewController {
         let green:CGFloat = CGFloat(drand48())
         let blue:CGFloat = CGFloat(drand48())
         
-        return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+        return UIColor(red:red, green: green, blue: blue, alpha: 0.5)
     }
 }
 
 /*
- view.backgroundColor
- view.transform = CGAffineTransform  scale, rotationAngle (pi, or pie / 2.0), mutation
- .concatenating(CGAffineTransform
- 
  might need this func
  func cleanup() {
  for subview in view.subviews {
@@ -98,5 +113,4 @@ class ViewController: UIViewController {
  subview.removeFromSuperview()
  }
  }
- 
  */
