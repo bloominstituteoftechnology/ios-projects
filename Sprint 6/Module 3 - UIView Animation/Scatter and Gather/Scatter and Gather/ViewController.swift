1aea77e897fdbf98fa68ac1d7d17e7843f0535a6
Audrey Welch <audreywelch@audreys-mbp.lan>
2019-01-09 14:01:27 -0800

    Labels and image added to interface builder

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -12,14 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+ 
+        
     }
 
     @IBAction func toggle(_ sender: Any) {
         
         var shouldScramble: Bool = false
         
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
+    @IBOutlet weak var lLabel: UILabel!
+    
+    @IBOutlet weak var aLabel: UILabel!
+    
+    @IBOutlet weak var mLabel: UILabel!
+    
+    @IBOutlet weak var bLabel: UILabel!
+    
+    @IBOutlet weak var dLabel: UILabel!
+    
+    @IBOutlet weak var a2Label: UILabel!
+    
+    @IBOutlet weak var lambdaImage: UIImageView!
+    
+    
+    
+    
+    
+    // Manually Creating Labels
+//    let font = UIFont(name: "American Typewriter", size: 50)
+//    let lLabel = UILabel(frame: CGRect(x: 50, y: 50, width: 50, height: 50))
+//    lLabel.translatesAutoresizingMaskIntoConstraints = false
+//    lLabel.text = "L"
+//    lLabel.font = font
+//    lLabel.textColor = #colorLiteral(red: 0.6823529412, green: 0.9960784314, blue: 0.8980392157, alpha: 1)
+//
+//    view.addSubview(lLabel)
 }
 
