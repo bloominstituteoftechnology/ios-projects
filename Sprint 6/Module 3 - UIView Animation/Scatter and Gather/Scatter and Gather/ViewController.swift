09610e41f08efe02f63278e7f61a88bb28a0c34c
Audrey Welch <audreywelch@audreys-mbp.lan>
2019-01-09 15:56:03 -0800

    Added label movements to scatter to random places on the screen

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -13,28 +13,72 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
- 
-        
     }
 
-    @IBAction func toggle(_ sender: Any) {
-        
-        var shouldScramble: Bool = false
+    // Disperses and animates
+    func scramble() {
         
         UIView.animate(withDuration: 2.0) {
             // Inside this closure, set property we want to animate (and how)  to its final value
-
+            
+            // Logo fade
             self.lambdaImage.alpha = 0.1
             
-            self.lLabel.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.7555645778, blue: 0.6809829309, alpha: 1)
-            self.aLabel.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.9884961752, blue: 0.5945744201, alpha: 1)
-            self.mLabel.backgroundColor = #colorLiteral(red: 0.8270156238, green: 0.9921568627, blue: 0.7935259539, alpha: 1)
-            self.mLabel.textColor = #colorLiteral(red: 0.9921568627, green: 0.8078820973, blue: 0.5530053243, alpha: 1)
-            self.bLabel.backgroundColor = #colorLiteral(red: 0.7157642889, green: 0.9921568627, blue: 0.9901249554, alpha: 1)
-            self.bLabel.textColor = #colorLiteral(red: 0.9921568627, green: 0.7020843945, blue: 0.8893551545, alpha: 1)
-            self.dLabel.backgroundColor = #colorLiteral(red: 0.6328204675, green: 0.8546559514, blue: 0.9921568627, alpha: 1)
-            self.a2Label.backgroundColor = #colorLiteral(red: 0.8699099479, green: 0.8043562504, blue: 0.9921568627, alpha: 1)
+            
+            // Color changes
+            let colorsArray = [#colorLiteral(red: 0.9921568627, green: 0.7555645778, blue: 0.6809829309, alpha: 1), #colorLiteral(red: 0.9921568627, green: 0.9884961752, blue: 0.5945744201, alpha: 1), #colorLiteral(red: 0.8270156238, green: 0.9921568627, blue: 0.7935259539, alpha: 1), #colorLiteral(red: 0.9921568627, green: 0.8078820973, blue: 0.5530053243, alpha: 1), #colorLiteral(red: 0.9921568627, green: 0.7020843945, blue: 0.8893551545, alpha: 1), #colorLiteral(red: 0.7094560989, green: 1, blue: 0.9118524051, alpha: 1), #colorLiteral(red: 0.7157642889, green: 0.9921568627, blue: 0.9901249554, alpha: 1), #colorLiteral(red: 0.9921568627, green: 0.8674790888, blue: 0.7740682373, alpha: 1), #colorLiteral(red: 0.6328204675, green: 0.8546559514, blue: 0.9921568627, alpha: 1), #colorLiteral(red: 0.8448194713, green: 0.9895079961, blue: 0.9921568627, alpha: 1), #colorLiteral(red: 0.8699099479, green: 0.8043562504, blue: 0.9921568627, alpha: 1)]
+            let colorsArrayText = [#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1), #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)]
+            
+            self.lLabel.textColor = colorsArrayText.randomElement()
+            self.aLabel.textColor = colorsArrayText.randomElement()
+            self.mLabel.textColor = colorsArrayText.randomElement()
+            self.bLabel.textColor = colorsArrayText.randomElement()
+            self.dLabel.textColor = colorsArrayText.randomElement()
+            self.a2Label.textColor = colorsArrayText.randomElement()
+            
+            self.lLabel.backgroundColor = colorsArray.randomElement()
+            self.aLabel.backgroundColor = colorsArray.randomElement()
+            self.mLabel.backgroundColor = colorsArray.randomElement()
+            self.bLabel.backgroundColor = colorsArray.randomElement()
+            self.dLabel.backgroundColor = colorsArray.randomElement()
+            self.a2Label.backgroundColor = colorsArray.randomElement()
+            
+            
+            // Label movements
+            self.lLabel.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.lLabel.frame.origin.y += CGFloat(Int.random(in: -100..<200))
+            
+            self.aLabel.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.aLabel.frame.origin.y += CGFloat(Int.random(in: -100..<200))
+            
+            self.mLabel.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.mLabel.frame.origin.y += CGFloat(Int.random(in: -100..<200))
+            
+            self.bLabel.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.bLabel.frame.origin.y += CGFloat(Int.random(in: -100..<200))
+            
+            self.dLabel.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.dLabel.frame.origin.y += CGFloat(Int.random(in: -100..<200))
+            
+            self.a2Label.frame.origin.x += CGFloat(Int.random(in: -100..<200))
+            self.a2Label.frame.origin.y += CGFloat(Int.random(in: -100..<200))
         }
+        
+    }
+    
+    // Returns to original size and placement
+    func gather() {
+        
+    }
+    
+    
+    @IBAction func toggle(_ sender: Any) {
+        
+        scramble()
+        
+        var shouldScramble: Bool = true
+        
+        
         
 //        let animation = CAKeyframeAnimation(keyPath: "lambdaLogoFade")
 //
