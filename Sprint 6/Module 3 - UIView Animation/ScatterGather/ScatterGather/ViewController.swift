6a689030d433be1f2f48a0ac55b3143f260f43cd
Frulwinn <frulwinn@gmail.com>
2019-01-09 09:34:36 -0800

    added toggle button item and connected it to viewcontroller

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  ScatterGather
-//
-//  Created by Lambda_School_Loaner_34 on 1/9/19.
-//  Copyright © 2019 Frulwinn. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func toggle(_ sender: Any) {
     }
-
-
 }
 
