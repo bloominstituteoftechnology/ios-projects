24f832761a623e033957ae8254bf0d8e25b7346d
Frulwinn <frulwinn@gmail.com>
2019-01-09 10:06:35 -0800

    added logo to assets, added logo and labels to IB

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -2,7 +2,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var lastALabel: UILabel!
+    
     @IBAction func toggle(_ sender: Any) {
+        var shouldScramble: Bool = true
     }
 }
-
