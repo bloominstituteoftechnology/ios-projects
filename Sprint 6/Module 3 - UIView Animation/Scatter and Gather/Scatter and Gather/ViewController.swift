ad32803f621054a08ee822240528ecd51f1df606
Audrey Welch <audreywelch@audreys-mbp.lan>
2019-01-09 14:46:26 -0800

    Animation added for changing of colors and fading of logo

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -21,17 +21,37 @@ class ViewController: UIViewController {
         
         var shouldScramble: Bool = false
         
+        UIView.animate(withDuration: 2.0) {
+            // Inside this closure, set property we want to animate (and how)  to its final value
 
+            self.lambdaImage.alpha = 0.1
+            
+            self.lLabel.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.7555645778, blue: 0.6809829309, alpha: 1)
+            self.aLabel.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.9884961752, blue: 0.5945744201, alpha: 1)
+            self.mLabel.backgroundColor = #colorLiteral(red: 0.8270156238, green: 0.9921568627, blue: 0.7935259539, alpha: 1)
+            self.mLabel.textColor = #colorLiteral(red: 0.9921568627, green: 0.8078820973, blue: 0.5530053243, alpha: 1)
+            self.bLabel.backgroundColor = #colorLiteral(red: 0.7157642889, green: 0.9921568627, blue: 0.9901249554, alpha: 1)
+            self.bLabel.textColor = #colorLiteral(red: 0.9921568627, green: 0.7020843945, blue: 0.8893551545, alpha: 1)
+            self.dLabel.backgroundColor = #colorLiteral(red: 0.6328204675, green: 0.8546559514, blue: 0.9921568627, alpha: 1)
+            self.a2Label.backgroundColor = #colorLiteral(red: 0.8699099479, green: 0.8043562504, blue: 0.9921568627, alpha: 1)
+        }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+//        let animation = CAKeyframeAnimation(keyPath: "lambdaLogoFade")
+//
+//        // keyFrames that will happen during the duration
+//        //animation.values = [lambdaImage.alpha = 0.1, lambdaImage.alpha = 1]
+//
+//        animation.duration = 3.0
+//
+//        CATransaction.begin()
+//
+//        lambdaImage.layer.add(animation, forKey: "lambdaLogoFadeAnimation")
+//
+//        CATransaction.setCompletionBlock {
+//            self.lambdaImage.alpha = 0.1
+//        }
+//
+//        CATransaction.commit()
         
         
         
