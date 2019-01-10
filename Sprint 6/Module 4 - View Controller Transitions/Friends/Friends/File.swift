bf9ca0be2e730a0d2ad037c18b93a3da22037373
Frulwinn <frulwinn@gmail.com>
2019-01-10 13:58:05 -0800

    fixed constraints on story board and added tableviewcell

---
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/File.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+struct Contact {
+    var name: String
+    var image: UIImage
+    var phoneNumber: String
+    
+}
---
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/TableViewCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+
+}
