a6ee9cbfc0294f8801b62e6b78ceffd21d0318f3
Frulwinn <frulwinn@gmail.com>
2019-01-10 14:09:58 -0800

    added Model file

--- Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/Model.swift
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/Model.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class Model {
+    static let shared = Model()
+    private init() {}
     
+    var contacts: [Contact] = []
+    
+    func count() -> Int {
+        return contacts.count
+    }
+    
+    func contact(forIndex index: Int) -> Contact {
+        return contacts[index]
+    }
 }
