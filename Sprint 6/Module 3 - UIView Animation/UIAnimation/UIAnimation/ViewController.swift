cb1d3a2b044bf3da7cb1e729db12338f6dc3066c
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-09 16:15:29 -0800

    Add some animations but still not requarments

--- Sprint 6/Module 3 - UIView Animation/UIAnimation/UIAnimation/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/UIAnimation/UIAnimation/ViewController.swift
@@ -12,9 +12,74 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+        view.addSubview(lambdaLogoImage)
+        
+        L.translatesAutoresizingMaskIntoConstraints = false
+        lambdaLogoImage.translatesAutoresizingMaskIntoConstraints = false
+        
+
     }
-
-
+    
+    var shouldScramble: Bool = true
+    
+    @IBOutlet weak var stack: UIStackView!
+    @IBOutlet weak var L: UILabel!
+    @IBOutlet weak var a: UILabel!
+    @IBOutlet weak var m: UILabel!
+    @IBOutlet weak var b: UILabel!
+    @IBOutlet weak var d: UILabel!
+    @IBOutlet weak var lastA: UILabel!
+    @IBOutlet weak var lambdaLogoImage: UIImageView!
+  
+    @IBOutlet weak var barButton: UIBarButtonItem!
+    
+    @IBAction func toggle(_ sender: Any) {
+    
+      animation()
+        shouldScramble = !shouldScramble
+    }
+    
+    
+   let numbers = Int.random(in: -250 ... 250)
+    
+    
+    
+    
+    func animation() {
+        let rect = CGRect(x: numbers, y: numbers, width: 50, height: 50)
+        let labelArray: [UILabel] = [L, a, m, b, d, lastA]
+        let point: CGPoint = CGPoint(x: 8, y: 85)
+        
+        let size: CGSize = CGSize(width: 53, height: 49)
+        for label in labelArray {
+        
+            if shouldScramble == true {
+            
+            UIView.animate(withDuration: 3) {
+            label.textColor = .white
+            label.tintColor = .black
+              
+           // label.bounds = rect
+                self.stack.layer.frame = CGRect(x: self.numbers, y: self.numbers, width: 50, height: 50)
+            label.backgroundColor = .red
+            
+                
+                
+                
+          //  var width = label.frame.size.width
+           // self.L.transform = CGAffineTransform(rotationAngle: self.rondomAngel)
+            
+            }
+            } else {
+                
+                label.backgroundColor = .white
+                label.textColor = .black
+                stack.frame = CGRect(origin: point, size: size)
+               // stack.transform = CGAffineTransform(translationX: CGFloat(8), y: CGFloat(72))
+                
+            }
+        }
+    }
 }
 
