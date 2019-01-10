f43e9bbea0b3ff2b5b626bca5989ffc7fc49b178
Frulwinn <frulwinn@gmail.com>
2019-01-10 14:31:06 -0800

    added image

--- Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/DetailViewController.swift
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/DetailViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var contactView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
     
 }
 
