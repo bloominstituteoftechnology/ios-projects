343797542bb94ef86b360087ea6fb430ff84fc4b
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-15 15:09:53 -0600

    New custom control class created

---
+++ Sprint 6/Module 2-Custom Controls/UICustom Control Async/UICustom Control Async/CustomControl.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    // Establish control as a value-providing and value-changing type
+    var value: Int = 1
+    
+}
--- Sprint 6/Module 2-Custom Controls/UICustom Control Async/UICustom Control Async/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/UICustom Control Async/UICustom Control Async/ViewController.swift
@@ -1,20 +1,32 @@
-//
-//  ViewController.swift
-//  UICustom Control Async
-//
-//  Created by Audrey Welch on 1/15/19.
-//  Copyright © 2019 Audrey Welch. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    let customControl = CustomControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateRating(customControl)
+        
     }
 
+    // Allows the control's client (the view controller) to receive updats about changes in the rating control
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+        let numberOfStars: Int = customControl.value
+        
+        if numberOfStars > 1 {
+            navigationItem.title = "User Rating: \(numberOfStars) stars"
+        } else {
+            navigationItem.title = "User Rating: \(numberOfStars) star"
+        }
+        
 
+        
+    }
+    
 }
 
