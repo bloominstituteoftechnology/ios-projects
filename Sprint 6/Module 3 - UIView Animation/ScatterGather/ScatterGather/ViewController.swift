bd875feb8603afeb6161925edbbec7610d0aff95
Frulwinn <frulwinn@gmail.com>
2019-01-16 13:12:57 -0800

    created view and labels in IB and connected them to viewController

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -3,6 +3,14 @@ import UIKit
 class ViewController: UIViewController {
     var shouldScramble: Bool = false
     
+    @IBOutlet weak var lambdaView: UIImageView!
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var aLastLabel: UILabel!
+    
     func scramble() {
         shouldScramble = !shouldScramble
     }
