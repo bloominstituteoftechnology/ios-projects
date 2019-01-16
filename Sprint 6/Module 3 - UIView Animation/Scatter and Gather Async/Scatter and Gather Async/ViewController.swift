97f1ec99d64a1cb84ab4a32bfa46f41d53842c55
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-16 14:39:04 -0600

    Toggle switch created

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather Async/Scatter and Gather Async/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather Async/Scatter and Gather Async/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
+    var shouldScramble: Bool = false
+    
+    @IBAction func toggle(_ sender: Any) {
+        
+        
+        
+    }
 }
 
