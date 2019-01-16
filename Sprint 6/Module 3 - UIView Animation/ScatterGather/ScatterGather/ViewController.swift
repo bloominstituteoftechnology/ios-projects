2c9eee423871e18d101ff35db05bdf9946038cc5
Frulwinn <frulwinn@gmail.com>
2019-01-16 12:48:11 -0800

    connected IBAction and added shouldScramble variable

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -1,6 +1,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var shouldScramble: Bool = false
+    
+    func scramble() {
+        shouldScramble = !shouldScramble
+    }
+    
+    @IBAction func toggle(_ sender: Any) {
+        if shouldScramble == false {
+            scatter()
+        } else {
+            gather()
+        }
+        scramble()
+    }
+    
+    func scatter() {
+        
+    }
+    
+    func gather() {
+        
+    }
     
     
 }
