48e8f00b9694bd5b5a0380459bca319d9052c276
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-16 16:03:23 -0600

    Labels and Image created programatically with constraints

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather Async/Scatter and Gather Async/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather Async/Scatter and Gather Async/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Scatter and Gather Async
-//
-//  Created by Audrey Welch on 1/16/19.
-//  Copyright © 2019 Audrey Welch. All rights reserved.
-//
 
 import UIKit
 
@@ -13,6 +6,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        createLabels()
+        
+        createLogo()
+        
     }
 
 
@@ -23,5 +20,121 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
+    var labelArray: [UILabel] = []
+    
+    let lambdaArray: [String] = ["L", "A", "M", "B", "D", "A"]
+    
+    func createLabels() {
+        
+        // Variable to increment spacing
+        var count: CGFloat = 1.0
+        
+        let width: CGFloat = 60
+        
+        for eachLetter in lambdaArray {
+            
+            // Create label
+            
+            let lambdaLabel = UILabel()
+            lambdaLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            let spacing: CGFloat = (width * count) + (8.0 * count)
+            
+            lambdaLabel.text = eachLetter
+            
+            lambdaLabel.font = UIFont.boldSystemFont(ofSize: 20)
+            
+            lambdaLabel.textColor = .black
+            
+            lambdaLabel.textAlignment = .center
+            
+            lambdaLabel.backgroundColor = .yellow
+            
+            view.addSubview(lambdaLabel)
+            
+            // Set constraints
+            
+            let widthConstraint = NSLayoutConstraint(item: lambdaLabel,
+                                                     attribute: .width,
+                                                     relatedBy: .equal,
+                                                     toItem: nil,
+                                                     attribute: .notAnAttribute,
+                                                     multiplier: 1.0,
+                                                     constant: 60)
+
+            let heightConstraint = NSLayoutConstraint(item: lambdaLabel,
+                                                     attribute: .height,
+                                                     relatedBy: .equal,
+                                                     toItem: lambdaLabel,
+                                                     attribute: .width,
+                                                     multiplier: 1.0,
+                                                     constant: 0.0)
+            
+            let topConstraint = NSLayoutConstraint(item: lambdaLabel,
+                                                   attribute: .top,
+                                                   relatedBy: .equal,
+                                                   toItem: view.safeAreaLayoutGuide,
+                                                   attribute: .top,
+                                                   multiplier: 1.0,
+                                                   constant: spacing)
+            
+            let centerXConstraint = NSLayoutConstraint(item: lambdaLabel,
+                                                       attribute: .centerX,
+                                                       relatedBy: .equal,
+                                                       toItem: view,
+                                                       attribute: .centerX,
+                                                       multiplier: 1.0,
+                                                       constant: 0)
+
+            NSLayoutConstraint.activate([widthConstraint, heightConstraint, topConstraint, centerXConstraint])
+            
+            labelArray.append(lambdaLabel)
+            
+            count += 1
+            
+        }
+    }
+
+    func createLogo() {
+        
+        let imageName = "Lambda_Logo_Full"
+        let logoImage = UIImage(named: imageName)
+        let logoImageView = UIImageView(image: logoImage!)
+        logoImageView.contentMode = .scaleAspectFit
+        
+        view.addSubview(logoImageView)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = NSLayoutConstraint(item: logoImageView,
+                                                 attribute: .leading,
+                                                 relatedBy: .equal,
+                                                 toItem: view,
+                                                 attribute: .leading,
+                                                 multiplier: 1.0,
+                                                 constant: 8)
+        
+        let trailingConstraint = NSLayoutConstraint(item: view,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: logoImageView,
+                                                    attribute: .trailing, multiplier: 1.0,
+                                                    constant: 8)
+        
+        let bottomConstraint = NSLayoutConstraint(item: logoImageView,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: view.safeAreaLayoutGuide,
+                                                  attribute: .bottom,
+                                                  multiplier: 1.0,
+                                                  constant: 60.0)
+        
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, bottomConstraint])
+        
+    }
+    
+    
 }
 
