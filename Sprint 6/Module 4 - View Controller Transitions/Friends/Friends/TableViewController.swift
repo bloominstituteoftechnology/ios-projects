53d04d29255a30f6c692808ab71cfcde130a7758
Frulwinn <frulwinn@gmail.com>
2019-01-17 14:09:08 -0800

    completed numberOfRowsInSection and cellForRowAt for TableViewController

--- Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/TableViewController.swift
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/TableViewController.swift
@@ -21,26 +21,20 @@ class TableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return friends.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("Unable to dequeue TableViewCell")}
 
-        // Configure the cell...
+        let friend = friends[indexPath.row]
+        cell.friendView?.image = friend.imageFriend
+        cell.nameLabel?.text = friend.name
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
