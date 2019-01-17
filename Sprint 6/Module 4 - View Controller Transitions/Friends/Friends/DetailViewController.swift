68dfcdc8f9d7bbcf36115edee66c0669c499d80a
Frulwinn <frulwinn@gmail.com>
2019-01-17 13:45:13 -0800

    hooked up connections for DetailViewController

--- Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/DetailViewController.swift
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/DetailViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var friendView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
