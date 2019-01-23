0af368e745f8672eae52a7b55a9e8e8c5c904846
AustinBCole <austin.cole.chileno@gmail.com>
2019-01-23 16:56:51 -0700

    Refactor code, still not working.

--- Sprint 7/Concurrency II (7.3)/HungryDevelopers.playground/Contents.swift
+++ Sprint 7/Concurrency II (7.3)/HungryDevelopers.playground/Contents.swift
@@ -1,87 +1,100 @@
 import UIKit
 
-let firstLock = NSLock()
-let secondLock = NSLock()
+let lock = NSLock()
+
 
 
 
 
 
 class Spoon {
-    var isPickedUp: Bool = false
+    
+    let id = UUID().uuidString
+    
     func pickUp() {
-        isPickedUp = true
+        lock.lock()
         return
     }
     func putDown() {
-        isPickedUp = false
+        lock.unlock()
         return
     }
 }
-
-var spoons : [Spoon] = []
-for _ in 1...5 {
-    let spoon = Spoon()
-    spoons.append(spoon)
-}
-
 
 class Developer {
-    
-    var id: Int?
+    var developer: String?
     var rightSpoon: Spoon?
     var leftSpoon: Spoon?
-    let dragonTalesIsCool = true
+    
     func think() {
-        guard let id = self.id else {return}
-        if id == 4 {
-            firstLock.lock()
-        spoons[id].pickUp
-            secondLock.lock()
-        spoons[0]
-        return
+        print("\(self.developer!) is thinking.")
+        if self.developer?.count == 5 {
+            print("\(self.developer!) is trying to pick up the right spoon.")
+            rightSpoon?.pickUp()
+            print("\(self.developer!) picked up the right spoon.")
+            print("\(self.developer!) is trying to pick up the left spoon.")
+            print(leftSpoon?.id)
+            leftSpoon?.pickUp()
+            print("\(self.developer!) picked up the left spoon.")
         } else {
-            firstLock.lock()
-            spoons[id].pickUp
-            secondLock.lock()
-            spoons[id + 1]
+            print("\(self.developer!) is trying to pick up the left spoon.")
+            print(leftSpoon?.id)
+            leftSpoon?.pickUp()
+            print("\(self.developer!) picked up the left spoon.")
+            print("\(self.developer!) is trying to pick up the right spoon.")
+            rightSpoon?.pickUp()
+            print(rightSpoon?.id)
+            print("\(self.developer!) picked up the right spoon.")
         }
+        
     }
     func eat() {
-        guard let id = self.id else {return}
+        print("\(self.developer!) is eating.")
         usleep(500)
-        if id == 4 {
-            
-            spoons[id].putDown()
-            firstLock.unlock()
-            
-            spoons[0].putDown()
-            secondLock.unlock()
-            return
-        } else {
-            spoons[id].putDown()
-            firstLock.unlock()
-            spoons[id + 1].putDown()
-            secondLock.unlock()
-        }
+        print("\(self.developer!) is trying to put down the right spoon.")
+        rightSpoon?.putDown()
+        print("\(self.developer!) put down the right spoon.")
+        print("\(self.developer!) is trying to put down the left spoon.")
+        leftSpoon?.putDown()
+        print("\(self.developer!) put down the left spoon.")
         return
     }
     func run() {
-        while dragonTalesIsCool {
+        while true {
             think()
             eat()
         }
     }
 }
 
+var spoons: [Spoon] = []
 var developers: [Developer] = []
-var id = -1
 for _ in 1...5 {
     let developer = Developer()
-    developer.id = id + 1
-    id += 1
+    let spoon = Spoon()
     developers.append(developer)
+    spoons.append(spoon)
 }
+
+
+
+developers[0].rightSpoon = spoons[0]
+developers[1].rightSpoon = spoons[1]
+developers[2].rightSpoon = spoons[2]
+developers[3].rightSpoon = spoons[3]
+developers[4].rightSpoon = spoons[4]
+
+developers[0].leftSpoon = spoons[1]
+developers[1].leftSpoon = spoons[2]
+developers[2].leftSpoon = spoons[3]
+developers[3].leftSpoon = spoons[4]
+developers[4].leftSpoon = spoons[0]
+
+developers[0].developer = "Austin"
+developers[1].developer = "Sam"
+developers[2].developer = "Jared"
+developers[3].developer = "Holly"
+developers[4].developer = "Rebecca"
 
 DispatchQueue.concurrentPerform(iterations: 5) {
     developers[$0].run()
