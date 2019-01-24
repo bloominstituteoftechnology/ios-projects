45273a71ec26ca6b81cfe684165e350811c03647
OmegaWulf <nikcxz@gmail.com>
2019-01-24 16:30:16 -0500

    Storyboard

---
+++ Sprint 11/Module 4/rpnCalcOBJC/rpnCalcOBJC/View Controllers/calcVC.h
@@ -0,0 +1,23 @@
+//
+//  calcVCViewController.h
+//  rpnCalcOBJC
+//
+//  Created by Nikita Thomas on 1/24/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface calcVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+
+@property (weak, nonatomic) IBOutlet UIButton *numTapped;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sprint 11/Module 4/rpnCalcOBJC/rpnCalcOBJC/View Controllers/calcVC.m
@@ -0,0 +1,41 @@
+//
+//  calcVCViewController.m
+//  rpnCalcOBJC
+//
+//  Created by Nikita Thomas on 1/24/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import "calcVC.h"
+
+@interface calcVC ()
+
+@end
+
+@implementation calcVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)numPressed:(id)sender {
+}
+
+- (IBAction)numPressed:(id)sender {
+}
+- (IBAction)numPressed:(UIButton *)sender {
+}
+- (IBAction)numPressed:(id)sender {
+}
+@end
