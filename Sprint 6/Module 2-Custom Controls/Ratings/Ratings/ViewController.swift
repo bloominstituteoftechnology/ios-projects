6f980e4e28d3df672554baa67c8d760fa8772df3
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-29 11:14:47 -0500

    Add  Xcode to project

---
+++ Sprint 6/Module 2-Custom Controls/Ratings/Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by Nelson Gonzalez on 1/29/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
