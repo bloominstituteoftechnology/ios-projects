975b99cdaa0092ca5eb9c2db84c83e1d2a656aa6
Frulwinn <frulwinn@gmail.com>
2019-01-29 12:47:13 -0800

    added variables and required init

--- Sprint 6/Module 2-Custom Controls/UIControl/UIControl/CustomControl.swift
+++ Sprint 6/Module 2-Custom Controls/UIControl/UIControl/CustomControl.swift
@@ -5,5 +5,17 @@ import UIKit
 class CustomControl: UIControl {
     
     var value: Int = 1
+    let componentDimension: CGFloat = 40.0
+    let componentCount = 5
+    let componentActiveColor = UIColor.black
+    let componentInactiveColor = UIColor.gray
     
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        setup()
+    }
+    
+    func setup() {
+        
+    }
 }
