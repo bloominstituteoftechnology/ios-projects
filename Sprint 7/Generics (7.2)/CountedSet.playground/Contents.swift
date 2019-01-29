286cd37e2b7e5d2458cf36f2c1fec656d0ebac84
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-01-29 14:10:15 -0800

    Project Setup

---
+++ Sprint 7/Generics (7.2)/CountedSet.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+
+enum Arrow { case iron, wooden, elven, dwarvish, magic, silver }
+var aCountedSet = CountedSet<Arrow>()
+aCountedSet[.iron] // 0
+var myCountedSet: CountedSet<Arrow> = [.iron, .magic, .iron, .silver, .iron, .iron]
+myCountedSet[.iron] // 4
+myCountedSet.remove(.iron) // 3
+myCountedSet.remove(.dwarvish) // 0
+myCountedSet.remove(.magic) // 0
