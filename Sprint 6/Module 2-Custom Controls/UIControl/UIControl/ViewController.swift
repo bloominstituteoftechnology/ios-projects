c8ac381eba7f87b8927c0ca6c46a6cce679851fa
Frulwinn <frulwinn@gmail.com>
2019-01-29 12:36:21 -0800

    implemented updateRating

--- Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/UIControl/UIControl/ViewController.swift
@@ -3,7 +3,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+        let n = ratingControl.value
+        
+        if n > 1 {
+            title = "User Rating: \(n) stars"
+        } else {
+            title = "User Rating: \(n) star"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
