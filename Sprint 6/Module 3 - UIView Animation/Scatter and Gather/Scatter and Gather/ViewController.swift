883ac2886a91605ab68e2e44c6e707714b173e6b
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-30 15:34:55 -0800

    application now has random color generator

--- Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -50,6 +50,14 @@ class ViewController: UIViewController {
             ])
     }
     
+    private func generateRandomColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
     @IBAction func toggle(_ sender: Any) {
         shouldScramble.toggle()
         
