94b9f815dd4ac75ea41beeb497ecf8067fe70ed3
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-30 14:05:02 -0800

    add rondom number and dev string to track

--- Sprint 7/Concurrency II (7.3)/Concurrency II.playground/Contents.swift
+++ Sprint 7/Concurrency II (7.3)/Concurrency II.playground/Contents.swift
@@ -2,6 +2,12 @@ import Foundation
 
 class Spoon {
     
+    var index: Int
+    
+    init(index: Int) {
+        self.index = index
+    }
+    
     private let lock = NSLock()
     
     func pickUp() {
@@ -20,20 +26,27 @@ class Developer {
     
     var leftSpoon: Spoon
     var rightSpoon: Spoon
-    
-    init(rightSpoon: Spoon, leftSpoon: Spoon){
+    var dev: String
+    init(rightSpoon: Spoon, leftSpoon: Spoon, dev: String){
         self.rightSpoon = rightSpoon
         self.leftSpoon = leftSpoon
+        self.dev = dev
     }
     
     
     func think() {
+        print("\(dev) is eating")
+         if leftSpoon.index > rightSpoon.index {
             rightSpoon.pickUp()
             leftSpoon.pickUp()
+         } else {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
         }
+    }
     
     func eat() {
-        usleep(500)
+        usleep(useconds_t(Int.random(in: 1...500)))
         rightSpoon.putDown()
         leftSpoon.putDown()
      
@@ -47,17 +60,17 @@ class Developer {
   }
 }
 
-let spoon1 = Spoon()
-let spoon2 = Spoon()
-let spoon3 = Spoon()
-let spoon4 = Spoon()
-let spoon5 = Spoon()
+let spoon1 = Spoon(index: 1)
+let spoon2 = Spoon(index: 2)
+let spoon3 = Spoon(index: 3)
+let spoon4 = Spoon(index: 4)
+let spoon5 = Spoon(index: 5)
 
-let developer1 = Developer(rightSpoon: spoon1, leftSpoon: spoon2)
-let developer2 = Developer(rightSpoon: spoon2, leftSpoon: spoon3)
-let developer3 = Developer(rightSpoon: spoon3, leftSpoon: spoon4)
-let developer4 = Developer(rightSpoon: spoon4, leftSpoon: spoon5)
-let developer5 = Developer(rightSpoon: spoon5, leftSpoon: spoon1)
+let developer1 = Developer(rightSpoon: spoon1, leftSpoon: spoon2, dev: "dev1")
+let developer2 = Developer(rightSpoon: spoon2, leftSpoon: spoon3, dev: "dev2")
+let developer3 = Developer(rightSpoon: spoon3, leftSpoon: spoon4, dev: "dev3")
+let developer4 = Developer(rightSpoon: spoon4, leftSpoon: spoon5, dev: "dev4")
+let developer5 = Developer(rightSpoon: spoon5, leftSpoon: spoon1, dev: "dev5")
 
 let developers = [developer1, developer2, developer3, developer4, developer5]
 
