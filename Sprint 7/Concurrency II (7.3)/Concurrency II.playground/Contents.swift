72cbe45752b73643ca110a32214874e06fb5ab2b
Sergey Osipyan <osipyansergey@gmail.com>
2019-01-30 15:38:24 -0800

    MVP

--- Sprint 7/Concurrency II (7.3)/Concurrency II.playground/Contents.swift
+++ Sprint 7/Concurrency II (7.3)/Concurrency II.playground/Contents.swift
@@ -1,8 +1,11 @@
 import Foundation
+import UIKit
 
 class Spoon {
     
     var index: Int
+    var endTime = Date().timeIntervalSinceReferenceDate
+    //var elapsedTime
     
     init(index: Int) {
         self.index = index
@@ -27,7 +30,9 @@ class Developer {
     var leftSpoon: Spoon
     var rightSpoon: Spoon
     var dev: String
-    init(rightSpoon: Spoon, leftSpoon: Spoon, dev: String){
+    
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon, dev: String) {
         self.rightSpoon = rightSpoon
         self.leftSpoon = leftSpoon
         self.dev = dev
@@ -36,24 +41,28 @@ class Developer {
     
     func think() {
         print("\(dev) is think ")
-         if leftSpoon.index > rightSpoon.index {
+         if leftSpoon.index < rightSpoon.index {
+            print("\(dev) is think until the left spoon is available")
+            leftSpoon.pickUp()
+            print("\(dev) pick up left spoon")
+            print("\(dev) is think until the right spoon is available")
+            rightSpoon.pickUp()
+            print("\(dev) pick up right spoon")
+            
+         } else {
             print("\(dev) is think until the right spoon is available")
             rightSpoon.pickUp()
             print("\(dev) pick up right spoon")
             print("\(dev) is think until the left spoon is available")
             leftSpoon.pickUp()
             print("\(dev) pick up left spoon")
-         } else {
-            leftSpoon.pickUp()
-            print("\(dev) pick up left spoon")
-            rightSpoon.pickUp()
-            print("\(dev) pick up right spoon")
+           
         }
     }
     
     func eat() {
         print("\(dev) is eating")
-        usleep(useconds_t(Int.random(in: 1...50_000)))
+        usleep(useconds_t(Int.random(in: 10000...150_000)))
         
         rightSpoon.putDown()
         print("\(dev) put down right spoon")
@@ -76,11 +85,11 @@ let spoon3 = Spoon(index: 3)
 let spoon4 = Spoon(index: 4)
 let spoon5 = Spoon(index: 5)
 
-let developer1 = Developer(rightSpoon: spoon1, leftSpoon: spoon2, dev: "dev1")
-let developer2 = Developer(rightSpoon: spoon2, leftSpoon: spoon3, dev: "dev2")
-let developer3 = Developer(rightSpoon: spoon3, leftSpoon: spoon4, dev: "dev3")
-let developer4 = Developer(rightSpoon: spoon4, leftSpoon: spoon5, dev: "dev4")
-let developer5 = Developer(rightSpoon: spoon5, leftSpoon: spoon1, dev: "dev5")
+let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2, dev: "don1")
+let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3, dev: "dev2")
+let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4, dev: "dev3")
+let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5, dev: "dev4")
+let developer5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1, dev: "dev5")
 
 let developers = [developer1, developer2, developer3, developer4, developer5]
 
