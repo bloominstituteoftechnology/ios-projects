a81ef375732d7c2fcf590b5bdcb08dc01a44e3c7
Frulwinn <frulwinn@gmail.com>
2019-01-30 12:30:00 -0800

    added toggle button in IB and hooked outlet to viewcontroller

--- Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterGather/ScatterGather/ViewController.swift
@@ -1,15 +1,11 @@
-//
-//  ViewController.swift
-//  ScatterGather
-//
-//  Created by Lambda_School_Loaner_34 on 1/30/19.
 //  Copyright © 2019 Frulwinn. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func toggle(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
