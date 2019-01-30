9c7bac2ed9b1ed4b9db71f9e5c723e6fa566b3dc
Angel <angelbuenrostro@outlook.com>
2019-01-30 16:27:11 -0500

    Add bool and IBAction

--- Sprint 6/Module 3 - UIView Animation/ScatterAndGather/ScatterAndGather/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var shouldScramble : Bool = false
+    
+    @IBAction func toggleButtonTapped(_ sender: Any) {
+        shouldScramble = !shouldScramble
+        
+        if shouldScramble {
+            // scramble the letters
+        } else {
+            // revert scrambling
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
