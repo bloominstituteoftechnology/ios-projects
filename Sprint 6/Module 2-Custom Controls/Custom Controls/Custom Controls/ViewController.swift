5c3263bbeadf2c12b0801467a3483d3391fb08df
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-29 20:14:25 -0800

    fix the view title generator
    - use the parameter passed into the function
    - correctly access "value" from the Custom Control class instead of creating one again

--- Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
@@ -2,13 +2,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func updateRating(_ sender: CustomControl) {
-        static var starCount = 0
-        if starCount == 1 {
-        self.navigationItem.title = "User Rating: 1 Star"
+    // Follows both steps 7 & 8 of the README where `sender` is changed to `ratingControl`
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        if ratingControl.value == 1 {
+            self.navigationItem.title = "User Rating: 1 Star"
         } else {
-            self.navigationItem.title = "User Rating: \(starCount) Stars"
+            self.navigationItem.title = "User Rating: \(ratingControl.value) Stars"
         }
     }
+    
 }
 
