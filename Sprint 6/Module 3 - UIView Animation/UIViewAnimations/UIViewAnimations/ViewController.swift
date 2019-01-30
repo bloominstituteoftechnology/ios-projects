554109c8497d09ce7719c9a2aaf7932423da8c82
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-30 15:31:04 -0500

    Set scenes with connections

--- Sprint 6/Module 3 - UIView Animation/UIViewAnimations/UIViewAnimations/ViewController.swift
+++ Sprint 6/Module 3 - UIView Animation/UIViewAnimations/UIViewAnimations/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Establish a boolean variable called shouldScramble that you toggle back and forth. You'll animate your views to either scramble them or gather them together when the toggle button is tapped.
+    var shouldScramble: Bool = false
+    
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var lastLetterLabel: UILabel!
+    @IBOutlet weak var lambdaLogoImageView: UIImageView!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
+    @IBAction func toggleBarButtonItemPressed(_ sender: UIBarButtonItem) {
+        if shouldScramble == false {
+        lambdaLogoImageView.alpha = 0
+           shouldScramble = true
+        } else {
+            lambdaLogoImageView.alpha = 1
+            shouldScramble = false
+        }
+    }
 }
 
