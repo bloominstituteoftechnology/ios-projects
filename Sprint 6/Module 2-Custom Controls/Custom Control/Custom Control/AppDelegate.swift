28dc0aad6d604b80f7261641882a9c02bbc78c3d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-31 07:34:53 -0500

    MVP. Allows users to rate by tapping stars.

---
+++ Sprint 6/Module 2-Custom Controls/Custom Control/Custom Control/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Custom Control
+//
+//  Created by Julian A. Fordyce on 1/30/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
