b1c108dee35c39e010a5fc334b1e2310cb8d7498
Frulwinn <frulwinn@gmail.com>
2019-01-31 14:02:44 -0800

    added friends

--- Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/TableViewController.swift
+++ Sprint 6/Module 4 - View Controller Transitions/Friends/Friends/TableViewController.swift
@@ -3,11 +3,21 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var friends: [Friend] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        let friend1 = Friend(name: "Wonder Woman", friendImage: UIImage(named: "WonderWoman")!, info: "Amazonian masterful athlete, acrobat, fighter and strategist")
+        
+        let friend2 = Friend(name: "Spider Woman", friendImage: UIImage(named: "SpiderWoman")!, info: "Superhuman strength, speed, reflexes, agility, endurance and durability")
+        
+        let friend3 = Friend(name: "Jean Grey", friendImage: UIImage(named: "JeanGray")!, info: "telepathic and telekinetic abilities")
+        
+        friends.append(friend1)
+        friends.append(friend2)
+        friends.append(friend3)
     }
 
     // MARK: - Table view data source
@@ -16,7 +26,7 @@ class TableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as? TableViewCell else { fatalError("Unable to dequeue TableViewCell") }
 
         // Configure the cell...
 
