2065ef3b31930ed5a8b239ec78a09b3acd89f92a
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-30 18:00:58 -0800

    update VC Nav Title for accuracy, since the loop starts from 0

--- Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
+++ Sprint 6/Module 2-Custom Controls/Custom Controls/Custom Controls/ViewController.swift
@@ -4,15 +4,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "Set Rating Below"
+        self.navigationItem.title = "Please Update Rating Below"
     }
     
     // Follows both steps 7 & 8 of the README where `sender` is changed to `ratingControl`
     @IBAction func updateRating(_ ratingControl: CustomControl) {
-        if ratingControl.value == 1 {
+        if ratingControl.value == 0 {
             self.navigationItem.title = "User Rating: 1 Star"
         } else {
-            self.navigationItem.title = "User Rating: \(ratingControl.value) Stars"
+            self.navigationItem.title = "User Rating: \(ratingControl.value + 1) Stars"
         }
     }
     
