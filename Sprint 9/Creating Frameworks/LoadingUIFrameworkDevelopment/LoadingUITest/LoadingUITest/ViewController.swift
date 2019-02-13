cb73141a59340630ba1849b9e73412f86b87406e
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-13 15:21:30 -0800

    Initial Commit

---
+++ Sprint 9/Creating Frameworks/LoadingUIFrameworkDevelopment/LoadingUITest/LoadingUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUITest
+//
+//  Created by Ivan Caldwell on 2/13/19.
+//  Copyright © 2019 Ivan Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
