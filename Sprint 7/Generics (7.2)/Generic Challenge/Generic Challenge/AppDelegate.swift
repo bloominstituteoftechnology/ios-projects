6fe03bf80f95344d7fe22c0082540d97b9411a40
Frulwinn <frulwinn@gmail.com>
2019-02-19 12:39:44 -0800

    initial commit

---
+++ Sprint 7/Generics (7.2)/Generic Challenge/Generic Challenge/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Generic Challenge
+//
+//  Created by Lambda_School_Loaner_34 on 2/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Sprint 7/Generics (7.2)/Generic Challenge/Generic Challenge/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Generic Challenge
+//
+//  Created by Lambda_School_Loaner_34 on 2/19/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+
+}
+
