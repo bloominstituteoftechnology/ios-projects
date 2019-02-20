2cac9677d3e116694351e2fd7de5ff8fa5a2af54
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-20 00:54:43 -0800

    four main methods are set up

--- Sprint 7/Generics (7.2)/generics Challenge.playground/Contents.swift
+++ Sprint 7/Generics (7.2)/generics Challenge.playground/Contents.swift
@@ -3,23 +3,29 @@ import Foundation
 
 struct CountedSet<Element: Hashable> {
     
-    func insert() {
-        
+    mutating func insert(element: Element) {
+        let value = storage[element] ?? 0
+            storage[element] = value + 1
     }
     
-    func remove() {
+    mutating func remove(element: Element) {
+        guard !storage.isEmpty else { return }
         
+        let value = storage[element] ?? 0
+        if value == 0 {
+            storage.removeValue(forKey: element)
+        } else {
+            storage[element] = value - 1
+        }
     }
     
-    subscript(_ member: Element) -> Int) {
-    
-        return 0
+    subscript(_ member: Element) -> Int {
+        return storage[member] ?? 0
     }
     
-    func count() {
-        
+    mutating func count() -> Int {
+        return storage.isEmpty ? 0 : storage.count
     }
     
     private(set) var storage: [Element : Int] = [:]
-    
 }
