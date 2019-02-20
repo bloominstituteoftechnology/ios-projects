d2843eebbdaf2b1e935b91346d5193c5f0471224
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-20 15:13:10 -0800

    gave deleopers name property to easily print while they are eating

--- Sprint 7/Concurrency II (7.3)/Hungry Developers.playground/Contents.swift
+++ Sprint 7/Concurrency II (7.3)/Hungry Developers.playground/Contents.swift
@@ -1,3 +1,65 @@
 import UIKit
 
+class Spoon {
+    
+    let lock = NSLock()
+    
+    func pickUp() {
+        lock.lock()
+    }
+    
+    func putDown() {
+        lock.unlock()
+    }
+
+}
+
+class Developer {
+    
+    let leftSpoon: Spoon
+    let rightSpoon: Spoon
+    let name: String
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon, name: String) {
+        (self.leftSpoon, self.rightSpoon, self.name) = (leftSpoon, rightSpoon, name)
+    }
+    
+    func think() {
+        leftSpoon.pickUp()
+        rightSpoon.pickUp()
+        
+        return
+    }
+    
+    func eat() {
+        print("\(name) is currently eating")
+        sleep(2)
+        
+        leftSpoon.putDown()
+        rightSpoon.putDown()
+    }
+    
+    func run() {
+        think()
+        eat()
+    }
+}
+
+let Spoon1 = Spoon()
+let Spoon2 = Spoon()
+let Spoon3 = Spoon()
+let Spoon4 = Spoon()
+let Spoon5 = Spoon()
+
+let Developer1 = Developer(leftSpoon: Spoon1, rightSpoon: Spoon2, name: "Developer1")
+let Developer2 = Developer(leftSpoon: Spoon2, rightSpoon: Spoon3, name: "Developer2")
+let Developer3 = Developer(leftSpoon: Spoon3, rightSpoon: Spoon4, name: "Developer3")
+let Developer4 = Developer(leftSpoon: Spoon4, rightSpoon: Spoon5, name: "Developer4")
+let Developer5 = Developer(leftSpoon: Spoon5, rightSpoon: Spoon1, name: "Developer5")
+
+let developers = [Developer1, Developer2, Developer3, Developer4, Developer5]
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+    developers[$0].run()
+}
 
