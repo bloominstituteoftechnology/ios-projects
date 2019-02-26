2cf972a4607d6bc54da4e52ee0513e2bbd594895
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-26 15:08:21 -0800

    plan out what to add

--- Sprint 7/Generics (7.2)/Generics Afternoon Project.playground/Contents.swift
+++ Sprint 7/Generics (7.2)/Generics Afternoon Project.playground/Contents.swift
@@ -2,4 +2,16 @@ import UIKit
 
 struct CountedSet<Element> where Element: Hashable {
     
+    // Insert element
+    
+    // Remove element
+    
+    // Insert or Remove Element
+    
+    // Support subscripting to look up current values, return 0 if one is not found.
+    
+    // Count the amount of elements in the set and check if it's empty.
+    
+    // Conform to ExpressibleByArrayLiteral, perhaps via an extension
+    
 }
