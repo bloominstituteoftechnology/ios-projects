c2b8d8b5e64b665d266ae8f59faca5a767312f72
Angel <angelbuenrostro@outlook.com>
2019-02-26 19:05:32 -0500

    Make Counted Set able to insert and remove items

--- Sprint 6/Module 4 - View Controller Transitions/FriendsAnimated/FriendsAnimated/Views/FriendsTableViewController.swift
+++ Sprint 6/Module 4 - View Controller Transitions/FriendsAnimated/FriendsAnimated/Views/FriendsTableViewController.swift
@@ -12,15 +12,29 @@ class FriendsTableViewController: UITableViewController {
     
     let friendController = FriendController()
 
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.backItem?.hidesBackButton = true
         
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        UserDefaults.standard.set("https://fluffy.es", forKey: "homepage")
+        UserDefaults.standard.set(false, forKey: "darkmode")
+        UserDefaults.standard.set(true, forKey: "user")
+        
+        let isUser = UserDefaults.standard.bool(forKey: "user")
+                if isUser {
+            print("user is true")
+        }
+        let library_path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]
+        let appSupportPath = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true)[0]
+        
+        print("library path is \(library_path)")
+        
+        
     }
 
     // MARK: - Table view data source
--- Sprint 7/Generics (7.2)/GenericsProject.playground/Contents.swift
+++ Sprint 7/Generics (7.2)/GenericsProject.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+struct CountedSet<Element: Hashable> {
+    
+    init(elements: [Element: Int]) {
+        self.dictionary = elements
+    }
+    
+    private var dictionary = Dictionary<Element, Int>()
+    
+    mutating func insert(_ element: Element){
+        let duplicateElements = self.dictionary.filter { $0.key == element }
+        self.dictionary[element] = duplicateElements.count+1
+    }
+    
+    mutating func removal(_ element: Element){
+        var newDictionary = self.dictionary
+        let elementDictionary = self.dictionary.filter { $0.key == element }
+        print(elementDictionary)
+        if elementDictionary[element]! > 1{
+            newDictionary[element] = (elementDictionary[element]! - 1)
+            self.dictionary = newDictionary
+            return
+        }
+        
+        newDictionary = self.dictionary.filter { $0.key != element }
+        
+        self.dictionary = newDictionary
+    }
+}
+
+var set1 = CountedSet(elements: ["a": 1, "b": 1, "c": 1])
+
+print(set1)
+set1.insert("d")
+set1.insert("d")
+print(set1)
+set1.removal("d")
+print(set1)
