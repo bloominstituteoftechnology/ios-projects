4a0e05d229487db206cd12de6843b793dfa00951
Sergey Osipyan <osipyansergey@gmail.com>
2019-02-28 14:30:11 -0800

    create new xCode project

---
+++ Sprint 11/Module 4/ObjcCalc/ObjcCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcCalc
+//
+//  Created by Sergey Osipyan on 2/28/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sprint 11/Module 4/ObjcCalc/ObjcCalc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcCalc
+//
+//  Created by Sergey Osipyan on 2/28/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
