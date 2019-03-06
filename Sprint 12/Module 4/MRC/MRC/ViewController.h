a760dc6ea66b220fd1ae346a15642255686e40a1
Ben Hakes <bhhakes@gmail.com>
2019-03-06 14:06:09 -0600

    add project

---
+++ Sprint 12/Module 4/MRC/MRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRC
+//
+//  Created by Benjamin Hakes on 3/6/19.
+//  Copyright © 2019 Benjamin Hakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sprint 12/Module 4/MRC/MRC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MRC
+//
+//  Created by Benjamin Hakes on 3/6/19.
+//  Copyright © 2019 Benjamin Hakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
