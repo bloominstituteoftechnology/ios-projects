c2b34b6ade364bda788b6df06c9822b280f3cada
Angel <angelbuenrostro@outlook.com>
2019-03-14 04:46:34 -0400

    Create Test App Project

---
+++ Sprint 7/Concurrency II (7.3)/HungryDevelopers/HungryDevelopers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HungryDevelopers
+//
+//  Created by Angel Buenrostro on 2/27/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sprint 9/Creating Frameworks/LoadingUiFrameworkDevelopment/LoadingUiTest/LoadingUiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingUiTest
+//
+//  Created by Angel Buenrostro on 3/14/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
