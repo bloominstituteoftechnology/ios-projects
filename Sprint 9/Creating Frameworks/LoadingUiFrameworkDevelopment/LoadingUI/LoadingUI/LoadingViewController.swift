bdeea17bb9050e22c4caee75511bfcf7518f64f0
Angel <angelbuenrostro@outlook.com>
2019-03-14 11:15:41 -0400

    Add example framework

---
+++ Sprint 9/Creating Frameworks/LoadingUiFrameworkDevelopment/LoadingUI/LoadingUI/LoadingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingViewController.swift
+//  LoadingUI
+//
+//  Created by Angel Buenrostro on 3/14/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func playAnimation(){
+        
+    }
+    
+    func stopAnimation(){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
