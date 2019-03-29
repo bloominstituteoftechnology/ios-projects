f7c01f8fd0bf0e51e88600aed942df2c75756b27
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-28 22:20:31 -0700

    digitaccumalator complete

---
+++ Sprint 11/Module 4/RPN Calculator Obj-C/RPN Calculator Obj-C/Model/MJRDigitAccumulator.m
@@ -0,0 +1,55 @@
+//
+//  MJRDigitAccumulator.m
+//  RPN Calculator Obj-C
+//
+//  Created by Moses Robinson on 3/28/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import "MJRDigitAccumulator.h"
+
+@interface MJRDigitAccumulator()
+
+@property NSMutableArray *digits;
+
+@end
+
+@implementation MJRDigitAccumulator
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _digits = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)addDigitWithNumericValue: (NSInteger)value
+{
+    if (value < 0 || value > 9) { return; }
+    
+    NSNumber *digit = [NSNumber numberWithInteger:value];
+    [self.digits addObject:digit];
+}
+
+- (void)addDecimalPoint
+{
+    if ([self.digits containsObject:@"."]) { return; }
+    
+    [self.digits addObject:@"."];
+}
+
+- (void)clear
+{
+    [self.digits removeAllObjects];
+}
+
+- (double)value
+{
+    NSString *numberString = [self.digits componentsJoinedByString:@""];
+    double number = [numberString doubleValue];
+    return number;
+}
+
+@end
--- Sprint 11/Module 4/RPN Calculator Obj-C/RPN Calculator Obj-C/View Controllers/MJRCalculatorViewController.m
+++ Sprint 11/Module 4/RPN Calculator Obj-C/RPN Calculator Obj-C/View Controllers/MJRCalculatorViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MJRCalculatorViewController.h"
+#import "MJRCalculator.h"
+#import "MJRDigitAccumulator.h"
 
 @interface MJRCalculatorViewController ()
 
@@ -16,17 +18,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
