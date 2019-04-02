8ede1edbce040a01b963220bff5e361e88033e80
Moses Robinson <mosesjrobinson@icloud.com>
2019-04-01 23:49:56 -0700

    connected views to storyboard

---
+++ Sprint 12/Modules 2&3/Astronomy - ObjC/Astronomy - ObjC/View Controllers/MJRPhotosCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  MJRPhotosCollectionViewController.h
+//  Astronomy - ObjC
+//
+//  Created by Moses Robinson on 4/1/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJRPhotosCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sprint 12/Modules 2&3/Astronomy - ObjC/Astronomy - ObjC/Views/MJRIMageCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MJRIMageCollectionViewCell.h
+//  Astronomy - ObjC
+//
+//  Created by Moses Robinson on 4/1/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJRIMageCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
