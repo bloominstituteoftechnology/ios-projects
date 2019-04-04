8318a16594a21b65a4cee0593be4557f97b7a6f4
Moses Robinson <mosesjrobinson@icloud.com>
2019-04-03 19:27:29 -0700

    initial commit

---
+++ Sprint 12/Module 4/Contacts MRC/Contacts MRC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts MRC
+//
+//  Created by Moses Robinson on 4/3/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
