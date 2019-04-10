417ecb449d001095c244248bbcefdb500631c278
Frulwinn <frulwinn@gmail.com>
2019-04-10 17:22:27 -0500

    initial commit

---
+++ Sprint 12/Module 4/Contacts/Contacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by Lambda_School_Loaner_34 on 4/10/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
