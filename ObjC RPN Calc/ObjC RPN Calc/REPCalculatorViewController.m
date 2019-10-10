831a3bf4ec0215381508ebb6a4afd2b2969d3df5
Michael Redig <mredig@gmail.com>
2019-10-10 18:50:25 -0500

    started vc logic

--- ObjC RPN Calc/ObjC RPN Calc/REPCalculatorViewController.m
+++ ObjC RPN Calc/ObjC RPN Calc/REPCalculatorViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "REPCalculatorViewController.h"
+#import "REPDigitAccumulator.h"
+#import "REPCalculator.h"
 
 @interface REPCalculatorViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 
-
+@property (readonly) REPDigitAccumulator* digitAccumulator;
+@property (readonly) REPCalculator* calculator;
 
 @end
 
@@ -20,38 +23,95 @@
 
 @implementation REPCalculatorViewController
 
+- (instancetype)initWithCoder:(NSCoder *)coder {
+	self = [super initWithCoder:coder];
+	if (self) {
+		[self configureInternals];
+	}
+	return self;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+		[self configureInternals];
+	}
+	return self;
+}
+
+- (void)configureInternals {
+	_digitAccumulator = [[REPDigitAccumulator alloc] init];
+	_calculator = [[REPCalculator alloc] init];
+}
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
 
+- (void)updateViews {
+	NSString* viewedText = @"";
+	if (self.digitAccumulator.stringValue.length > 0) {
+		viewedText = self.digitAccumulator.stringValue;
+	} else if ([self.calculator topValue]) {
+		viewedText = self.calculator.topValue.stringValue;
+	}
+	self.textField.text = viewedText;
+}
+
+- (void)addNumberToStack {
+	if (self.digitAccumulator.stringValue.length == 0) {
+		return;
+	}
+
+	[self.calculator pushNumber:self.digitAccumulator.value];
+	[self.digitAccumulator clear];
+	[self updateViews];
+}
+
 
 - (IBAction)numberButtonPressed:(UIButton *)sender {
+	[self.digitAccumulator addDigitWithNumericValue:(int)sender.tag];
+	[self updateViews];
 }
 
 - (IBAction)decimalButtonPressed:(UIButton *)sender {
+	[self.digitAccumulator addDecimalPoint];
+	[self updateViews];
 }
 
 - (IBAction)subtractButtonPressed:(UIButton *)sender {
+	[self addNumberToStack];
+	[self.calculator applyOperator:subtract];
+	[self updateViews];
 }
 
 - (IBAction)addButtonPressed:(UIButton *)sender {
+	[self addNumberToStack];
+	[self.calculator applyOperator:add];
+	[self updateViews];
 }
 
 - (IBAction)multiplyButtonPressed:(UIButton *)sender {
+	[self addNumberToStack];
+	[self.calculator applyOperator:multiply];
+	[self updateViews];
 }
 
 - (IBAction)divideButtonPressed:(UIButton *)sender {
+	[self addNumberToStack];
+	[self.calculator applyOperator:multiply];
+	[self updateViews];
 }
 
 - (IBAction)clearButtonPressed:(UIButton *)sender {
+	[self.digitAccumulator clear];
+	[self.calculator clear];
+	[self updateViews];
 }
 
 - (IBAction)returnButtonPressed:(UIButton *)sender {
+	[self addNumberToStack];
+	[self updateViews];
 }
-
-
-
-
 
 @end
