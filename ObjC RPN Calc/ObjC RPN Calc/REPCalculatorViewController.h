9cf20a7d56e227326bd74f5c4a6e117b647c95a6
Michael Redig <mredig@gmail.com>
2019-10-10 16:51:30 -0500

    hooked up storyboard to code

--- ObjC RPN Calc/ObjC RPN Calc/REPCalculatorViewController.h
+++ ObjC RPN Calc/ObjC RPN Calc/REPCalculatorViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  REPCalculatorViewController.h
 //  ObjC RPN Calc
 //
 //  Created by Michael Redig on 10/10/19.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface REPCalculatorViewController : UIViewController
 
 
 @end
---
+++ ObjC RPN Calc/ObjC RPN Calc/REPCalculatorViewController.m
@@ -0,0 +1,57 @@
+//
+//  REPCalculatorViewController.m
+//  ObjC RPN Calc
+//
+//  Created by Michael Redig on 10/10/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import "REPCalculatorViewController.h"
+
+@interface REPCalculatorViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+
+@end
+
+
+
+@implementation REPCalculatorViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+- (IBAction)numberButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)decimalButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)subtractButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)addButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)multiplyButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)divideButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)clearButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)returnButtonPressed:(UIButton *)sender {
+}
+
+
+
+
+
+@end
